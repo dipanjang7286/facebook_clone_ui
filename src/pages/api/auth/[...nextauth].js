980913ad9a68d3c1987182e91d8c49d9689d5082
dipanjang7286 <71178502+dipanjang7286@users.noreply.github.com>
2023-04-08 11:42:11 +0530

    authentication added

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import FcaebookProvider from "next-auth/providers/facebook"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        FcaebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ],
+}
+
+export default NextAuth(authOptions)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../../components/Header'
 import Sidebar from '../../components/Sidebar'
+import Login from '../../components/Login'
+import { getSession } from 'next-auth/react'
 
-export default function Home ()
+export default function Home ( {session} )
 {
+  if (!session) return <Login></Login>;
   return (
     <div>
       <Head>
@@ -17,4 +20,11 @@ export default function Home ()
     </div>
     
   )
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession(context)
+  return {
+    props:{ session },
+  }
 }
