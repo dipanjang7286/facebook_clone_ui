9268ce2640605d1056dff4a2bd37d95c98bc5269
dipanjang7286 <71178502+dipanjang7286@users.noreply.github.com>
2023-04-23 10:08:38 +0530

    new modification added

--- public/src/features/postSlice.js
+++ public/src/features/postSlice.js
@@ -8,12 +8,12 @@ export const postSlice = createSlice({
         addPost: (state, action) =>{
             state.value.unshift(action.payload)
         },
-        addAllPost: (state, action) =>{
-            state.value.push(...action.payload)
+        fetchAllPost: (state, action) =>{
+            state.value = (action.payload)
         },
     },
 });
 
-export const {addPost, addAllPost} = postSlice.actions;
+export const {addPost, fetchAllPost} = postSlice.actions;
 export const selectPost = (state) => state.post.value;
 export default postSlice.reducer;
